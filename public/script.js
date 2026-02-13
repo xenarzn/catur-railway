0f086996e4ef560733661aa7b68dea6a1047a34e
xenarzn <xenarzn@gmail.com>
2026-02-13 21:00:24 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,72 +3,82 @@ var game = new Chess();
 var socket = io();
 var $status = $('#status');
 var $playerSide = $('#playerSide');
-var playerRole = null; // 'w' untuk putih, 'b' untuk hitam
+var playerRole = null;
 
-// Saat pertama kali terhubung, tentukan siapa kita
-socket.on('connect', function() {
-    $status.html('Terhubung ke Server');
-});
+// Fungsi Suara
+function playSound(type) {
+    if (type === 'move') document.getElementById('moveSound').play();
+    if (type === 'check') document.getElementById('checkSound').play();
+}
 
-// Logika untuk menentukan posisi papan dan peran pemain
-socket.on('playerRole', function(role) {
-    playerRole = role;
-    var sideName = (role === 'w') ? 'Putih' : 'Hitam';
-    $playerSide.html('Anda bermain sebagai: <b>' + sideName + '</b>');
-    
-    // Putar papan jika kita adalah pemain Hitam
-    if (role === 'b') {
-        board.orientation('black');
-    }
-});
+// Fungsi Hint Langkah
+function removeHints() {
+    $('.highlight-hint').remove();
+}
 
-function onDragStart (source, piece, position, orientation) {
-    if (game.game_over()) return false;
+function showHints(square) {
+    var moves = game.moves({ square: square, verbose: true });
+    if (moves.length === 0) return;
 
-    // Cegah pemain menggerakkan bidak lawan
-    if ((playerRole === 'w' && piece.search(/^b/) !== -1) ||
-        (playerRole === 'b' && piece.search(/^w/) !== -1) ||
-        (game.turn() !== playerRole)) {
-        return false;
+    for (var i = 0; i < moves.length; i++) {
+        var $square = $('.square-' + moves[i].to);
+        $square.append('<div class="highlight-hint" style="position:absolute; top:50%; left:50%; transform:translate(-50%, -50%); pointer-events:none;"></div>');
     }
 }
 
-function onDrop (source, target) {
-    var move = game.move({
-        from: source,
-        to: target,
-        promotion: 'q'
-    });
+socket.on('connect', () => $status.html('Sistem Siap'));
 
+socket.on('playerRole', function(role) {
+    playerRole = role;
+    if (role === 'observer') {
+        $playerSide.html('Anda adalah <b>Penonton</b>');
+    } else {
+        var sideName = (role === 'w') ? 'Putih' : 'Hitam';
+        $playerSide.html('Anda bermain sebagai: <b>' + sideName + '</b>');
+        if (role === 'b') board.orientation('black');
+    }
+});
+
+function onDragStart(source, piece) {
+    if (game.game_over() || playerRole === 'observer') return false;
+    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1) ||
+        (game.turn() !== playerRole)) {
+        return false;
+    }
+    showHints(source);
+}
+
+function onDrop(source, target) {
+    removeHints();
+    var move = game.move({ from: source, to: target, promotion: 'q' });
     if (move === null) return 'snapback';
 
+    playSound(game.in_check() ? 'check' : 'move');
     updateStatus();
     socket.emit('langkah', { move: move, board: game.fen() });
 }
 
-function onSnapEnd () {
+function onSnapEnd() {
     board.position(game.fen());
 }
 
-socket.on('langkah', function (data) {
+socket.on('langkah', function(data) {
     game.load(data.board);
     board.position(game.fen());
+    playSound(game.in_check() ? 'check' : 'move');
     updateStatus();
 });
 
-function updateStatus () {
+function updateStatus() {
     var status = '';
     var moveColor = (game.turn() === 'b') ? 'Hitam' : 'Putih';
 
-    if (game.in_checkmate()) {
-        status = 'Game Over, ' + moveColor + ' Skakmat.';
-    } else if (game.in_draw()) {
-        status = 'Game Over, Remis.';
-    } else {
+    if (game.in_checkmate()) { status = 'SKAKMAT! ' + moveColor + ' kalah.'; }
+    else if (game.in_draw()) { status = 'REMIS (Seri).'; }
+    else {
         status = 'Giliran: ' + moveColor;
-        if (game.in_check()) {
-            status += ' (Skak!)';
-        }
+        if (game.in_check()) status += ' (Skak!)';
     }
     $status.html(status);
 }
